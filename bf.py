2e7256f1003196e1988394dfc5dacc1f8026931f
Krish Munot <krishmunot@gmail.com>
2016-05-27 21:01:38 +0000

    adding basic bf cmds

--- bf.py
+++ bf.py
@@ -1,2 +1,49 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+import sys
+
+# simulate the tape with a list
+tape = [0]*(3*10**4)
+
+# the data pointer
+ptr   = 0
+
+# the simple brainfuck commands
+
+def lt():
+    """ performs a < """
+    global ptr
+    if ptr <= 0:
+        raise ValueError, "Segmentation fault!"
+    ptr -= 1
+
+def rt():
+    """" performs a > """
+    global ptr
+    if ptr >= 3*10**4 - 1:
+        raise ValueError, "Segmentation fault!"
+    ptr += 1
+
+def plus():
+    """ performs a + """
+    global ptr
+    tape[ptr] = (tape[ptr] + 1) % 256
+
+def minus():
+    """ perfoms a - """
+    global ptr
+    tape[ptr] = (tape[ptr] - 1) % 256
+
+def dot():
+    """ performs a . """
+    global ptr
+    sys.stdout.write(chr(tape[ptr]))
+
+def comma():
+    """ performs a , """
+    global ptr
+    c = ord(sys.stdin.read(1))
+    if c != 26:
+        tape[ptr] = c
+        
